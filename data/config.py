dc6f574f8ee145c724f395c4199ff9ad199a5209
SaadDadaxon <shdadaxon99@gmail.com>
2023-08-19 22:14:43 +0500

    config

--- data/config.py
+++ data/config.py
@@ -1,10 +1,4 @@
-from environs import Env
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
-
-# .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = "5966956630:AAFr7iVMop_rKd-UPMoAYxOMjupCKIkzXEA"  # Bot toekn
 ADMINS = [1004294567, ]  # adminlar ro'yxati
 IP = "localhost"  # Xosting ip manzili
